4b195e1cc0faf5b0f5b866a91cc10b3c18371f47
Spencer Johnson <sljohnson2222@gmail.com>
2022-03-14 22:49:54 -0500

    commit tree 4a11d57d862b1a1649e58b43c6f042517ba74527

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
 
+class Book
 
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+    
+    
+    def initialize(title)
+        @title = title 
+    end
+
+    # def properties(author)
+    #     @author = author
+    # end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+# b1 = Book.new
+# b1.author = "Agatha Christie"
+# b1.title = "And Then There Were None"
